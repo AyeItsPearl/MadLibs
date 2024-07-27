34d6d9fe8e1c08e000117a8d26a2582cfa616938
AyeItsPearl <realayeitspearl@gmail.com>
2024-07-27 22:45:10 +0800

    first commit

---
+++ Mad Libs/madlibs.py
@@ -0,0 +1,10 @@
+print("MADLIB")
+
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous Person: ")
+madlib = f"Computer Programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2}, like you are {famous_person}!"
+        
+print(madlib)
